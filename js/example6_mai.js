6e68066567b72cac5b59af1bc2967ab31d57e946
molabarrieta <61632299+molabarrieta@users.noreply.github.com>
2021-02-24 13:42:38 -0500

    Update example6_mai.js

--- js/example6_mai.js
+++ js/example6_mai.js
@@ -1,11 +1,11 @@
 var startDate = new Date();
 startDate.setUTCHours(0, 0, 0, 0);
 
-//var today = new Date();
-//var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-//var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-//var dateTime = date+'T'+time+'Z';
-//var date_end = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+(today.getDate()+2);
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+'T'+time+'Z';
+var date_end = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+(today.getDate()+2);
 
 var map = L.map('map', {
     zoom: 10,
@@ -21,8 +21,8 @@ var map = L.map('map', {
     timeDimensionOptions: {
         timeInterval: "2021-02-23:06:00Z/2021-02-25:06:00Z",
         period: "PT1H",
-        currentTime: Date.parse("2021-02-24T06:00:00Z")
-        //currentTime: Date.parse(dateTime)
+        //currentTime: Date.parse("2021-02-24T06:00:00Z")
+        currentTime: Date.parse(dateTime)
     },
     center: [29.8, -81.2]
 });
