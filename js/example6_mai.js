13d7fb16aa1995fc37458a071af2ea4aeddede37
molabarrieta <61632299+molabarrieta@users.noreply.github.com>
2021-06-11 15:19:51 -0400

    Update example6_mai.js

--- js/example6_mai.js
+++ js/example6_mai.js
@@ -9,7 +9,7 @@ var dateTime = date+'T'+time+'Z';
 
 var map = L.map('map', {
     //zoom: 10,
-    zoom: 7,
+    zoom: 9,
     fullscreenControl: true,
     timeDimensionControl: true,
     timeDimensionControlOptions: {
@@ -20,7 +20,7 @@ var map = L.map('map', {
     },
     timeDimension: true,
     timeDimensionOptions: {
-        timeInterval: "2021-01-01:00:00Z/2021-12-31:00:00Z",
+        timeInterval: "2021-06-11:00:00Z/2021-06-14:00:00Z",
         //timeInterval: TimeInt,
         period: "PT1H",
         //currentTime: Date.parse("2021-02-03T06:00:00Z")
