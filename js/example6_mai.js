92c4bc14adfde06ae3084f2033764d9924324517
molabarrieta <61632299+molabarrieta@users.noreply.github.com>
2021-06-17 14:28:39 -0400

    Add files via upload

--- js/example6_mai.js
+++ js/example6_mai.js
@@ -9,7 +9,7 @@ var dateTime = date+'T'+time+'Z';
 
 var map = L.map('map', {
     //zoom: 10,
-    zoom: 9,
+    zoom: 10,
     fullscreenControl: true,
     timeDimensionControl: true,
     timeDimensionControlOptions: {
@@ -27,14 +27,14 @@ var map = L.map('map', {
         currentTime: Date.parse(dateTime)
     },
     //center: [29.8, -81.2]
-    center: [26.75, -82.0]
+    center: [26.72, -82.0]
 });
 
 //var sapoWMS = "https://icoast.rc.ufl.edu/thredds/wms/roms_his_agg/AGG_ROMS_HIS.nc";
 // var sapoWMS = "https://icoast.rc.ufl.edu/thredds/wms/coawst/gtm/forecast/GTM_FORECAST_best.ncd";
 var sapoWMS= "https://icoast.rc.ufl.edu/thredds/wms/coawst/snb/forecast/SNB_FORECAST_best.ncd";
 
-var sapoHeightLayer = L.tileLayer.wms(sapoWMS, {
+var sapoWLLayer = L.tileLayer.wms(sapoWMS, {
     layers: 'zeta',
     format: 'image/png',
     transparent: true,
@@ -43,7 +43,7 @@ var sapoHeightLayer = L.tileLayer.wms(sapoWMS, {
     belowmincolor: "extend",
 });
 
-var sapoMeanDirectionLayer = L.nonTiledLayer.wms(sapoWMS, {
+var sapoWHLayer = L.nonTiledLayer.wms(sapoWMS, {
     layers: 'Hwave',
     format: 'image/png',
     transparent: true,
@@ -52,23 +52,47 @@ var sapoMeanDirectionLayer = L.nonTiledLayer.wms(sapoWMS, {
     belowmincolor: "extend",
 });
 
+var sapoSSLayer = L.nonTiledLayer.wms(sapoWMS, {
+    layers: 'salt',
+    format: 'image/png',
+    transparent: true,
+    colorscalerange: '0,35',
+    abovemaxcolor: "extend",
+    belowmincolor: "extend",
+});
 
+var sapoSTLayer = L.nonTiledLayer.wms(sapoWMS, {
+    layers: 'temp',
+    format: 'image/png',
+    transparent: true,
+    colorscalerange: '18,28',
+    abovemaxcolor: "extend",
+    belowmincolor: "extend",
+});
 
 var proxy = 'server/proxy.php';
-var sapoHeightTimeLayer = L.timeDimension.layer.wms(sapoHeightLayer, {
+var sapoWLTimeLayer = L.timeDimension.layer.wms(sapoWLLayer, {
     proxy: proxy,
     updateTimeDimension: false
 });
 
-var sapoMeanDirectionTimeLayer = L.timeDimension.layer.wms(sapoMeanDirectionLayer, {
+var sapoWHTimeLayer = L.timeDimension.layer.wms(sapoWHLayer, {
     proxy: proxy
 });
+
+var sapoSSTimeLayer = L.timeDimension.layer.wms(sapoSSLayer, {
+    proxy: proxy
+});
+
+var sapoSTTimeLayer = L.timeDimension.layer.wms(sapoSTLayer, {
+    proxy: proxy
+});
+
 /*
 var sapoPeakDirectionTimeLayer = L.timeDimension.layer.wms(sapoPeakDirectionLayer, {
     proxy: proxy
 });
 */
-
 
 var sapoLegend = L.control({
     position: 'bottomright'
@@ -82,10 +106,11 @@ sapoLegend.onAdd = function(map) {
     return div;
 };
 
-var sapoMeanDirectionLegend = L.control({
+var sapoWHLegend = L.control({
     position: 'bottomleft'
 });
-sapoMeanDirectionLegend.onAdd = function(map) {
+
+sapoWHLegend.onAdd = function(map) {
     var src = sapoWMS + "?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetLegendGraphic&LAYER=Hwave&colorscalerange=0,3&PALETTE=rainbow";
     var div = L.DomUtil.create('div', 'info legend');
     div.innerHTML +=
@@ -93,16 +118,46 @@ sapoMeanDirectionLegend.onAdd = function(map) {
     return div;
 };
 
+var sapoSSLegend = L.control({
+    position: 'bottomleft'
+});
+
+sapoSSLegend.onAdd = function(map) {
+    var src = sapoWMS + "?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetLegendGraphic&LAYER=salt&colorscalerange=0,35&PALETTE=rainbow";
+    var div = L.DomUtil.create('div', 'info legend');
+    div.innerHTML +=
+        '<img src="' + src + '" alt="legend">';
+    return div;
+};
+
+var sapoSTLegend = L.control({
+    position: 'bottomright'
+});
+
+sapoSTLegend.onAdd = function(map) {
+    var src = sapoWMS + "?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetLegendGraphic&LAYER=temp&colorscalerange=18,28&PALETTE=rainbow";
+    var div = L.DomUtil.create('div', 'info legend');
+    div.innerHTML +=
+        '<img src="' + src + '" alt="legend">';
+    return div;
+};
+
 var overlayMaps = {
-    "Icoast - water levels": sapoHeightTimeLayer,
-    "Icoast - wave height": sapoMeanDirectionTimeLayer
+    "Icoast - water levels": sapoWLTimeLayer,
+    "Icoast - wave height": sapoWHTimeLayer,
+    "Icoast - surface salinity": sapoSSTimeLayer,
+    "Icoast - surface temperature": sapoSTTimeLayer
 };
 
 map.on('overlayadd', function(eventLayer) {
     if (eventLayer.name == 'Icoast - water levels') {
         sapoLegend.addTo(this);
     } else if (eventLayer.name == 'Icoast - wave height') {
-        sapoMeanDirectionLegend.addTo(this);
+        sapoWHLegend.addTo(this);
+    } else if (eventLayer.name == 'Icoast - surface salinity') {
+        sapoSSLegend.addTo(this);
+    } else if (eventLayer.name == 'Icoast - surface temperature') {
+        sapoSTLegend.addTo(this);
     }
 });
 
@@ -110,12 +165,23 @@ map.on('overlayremove', function(eventLayer) {
     if (eventLayer.name == 'Icoast - water levels') {
         map.removeControl(sapoLegend);
     } else if (eventLayer.name == 'Icoast - wave height') {
-        map.removeControl(sapoMeanDirectionLegend);
+        map.removeControl(sapoWHLegend);
+	} else if (eventLayer.name == 'Icoast - surface salinity') {
+        map.removeControl(sapoSSLegend);
+	} else if (eventLayer.name == 'Icoast - surface temperature') {
+        map.removeControl(sapoSTLegend);
     }
 });
 
 var baseLayers = getCommonBaseLayers(map); // see baselayers.js
 L.control.layers(baseLayers, overlayMaps).addTo(map);
 
-sapoHeightTimeLayer.addTo(map);
-sapoPeakDirectionTimeLayer.addTo(map);
+/*
+sapoWLTimeLayer.addTo(map);
+sapoWHTimeLayer.addTo(map);
+sapoSSTimeLayer.addTo(map);
+*/
+
+sapoSSTimeLayer.addTo(map);
+
+
