b67d0197a7ad781e075969d72e0928ca17bf9877
molabarrieta <61632299+molabarrieta@users.noreply.github.com>
2021-12-10 09:24:30 -0500

    Update example6_mai.js

--- js/example6_mai.js
+++ js/example6_mai.js
@@ -18,7 +18,7 @@ var TimeInt = date+'/'+date5;
 
 var map = L.map('map', {
     //zoom: 10,
-    zoom: 9.5,
+    zoom: 9.25,
     fullscreenControl: true,
     timeDimensionControl: true,
     timeDimensionControlOptions: {
